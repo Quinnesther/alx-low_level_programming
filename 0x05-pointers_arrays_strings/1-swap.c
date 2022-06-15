7aa315af7cda6fcd77b7c4beeb32ecd66ec49e85
Quinnesther <chidinmachizoma2019@gmail.com>
2022-06-15 15:37:55 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - does swapping of memory contents
+ * @a: parameter
+ * @b: parameter
+ *
+ * Return: void.
+ */
+
+void swap_int(int *a, int *b)
+{
+	
+int x = *a;
+	*a = *b;
+	*b = x;
+}
